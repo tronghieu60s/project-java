d4ea3e1ee4ea212725733fb6c3a619dd266c2d07
unknown <truongth2904@gmail.com>
2020-12-21 21:39:51 +0700

    Main system.out

--- src/ClientSocket/Main.java
+++ src/ClientSocket/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         GameInterface gameInterface = new GameInterface();
-
+        System.out.println("ClientSocket.Main.main()");
         String sentence_to_server;
         String sentence_from_server;
 
